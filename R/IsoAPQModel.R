4c43e2450ab9ecfda42b58dc79c20afb6eff87b5
mstaniak <mtst@mstaniak.pl>
2021-05-26 11:45:27 +0200

    re-organized files

--- R/IsoAPQModel.R
+++ R/IsoAPQModel.R
@@ -1,18 +1,3 @@
-fit_iso_full_model = function(model_design) {
-    grad_m = getIsoAnalyticalGradient(model_design)
-    loglik_m = getIsoLogLikelihood(model_design)
-    num_random_effects = getIsoNumRandom(model_design)
-    num_fixed_effects = getIsoNumFixed(model_design)
-    num_proteins = getIsoNumProteins(model_design)
-    starting_values = c(0.1, rep(0.5, num_random_effects),
-                        rep(12, num_proteins),
-                        rep(1, num_fixed_effects))
-    # TODO: getIsoStartingValues
-    optimized_loglik = optim(starting_values, loglik_m, grad_m, method = "BFGS")
-    optimized_loglik
-}
-
-
 setClass("IsoAPQModel",
          slots = c(model_design = "IsoAPQModelDesign",
                    fitted_model_raw = "ANY",
@@ -81,59 +66,3 @@ setGeneric("getIsoLoglik", function(x) standardGeneric("getIsoLoglik"))
 setMethod("getIsoLoglik", "IsoAPQModel", function(x) x@loglik)
 setGeneric("getIsoFitStatus", function(x) standardGeneric("getIsoFitStatus"))
 setMethod("getIsoFitStatus", "IsoAPQModel", function(x) x@message)
-
-
-get_full_model_information = function(fitted_model, model_design) {
-    response = getIsoResponse(model_design)
-    num_fixed = getIsoNumFixed(model_design)
-    num_random = getIsoNumRandom(model_design)
-    num_proteins = getIsoNumProteins(model_design)
-
-    if (fitted_model[["convergence"]] == 0) {
-        pars = fitted_model[["par"]]
-        loglik = -fitted_model[["value"]]
-        coef = c(exp(pars[1]), pars[-1])
-        sigma = coef[1]
-        random_effects_var = (sigma ^ 2) * coef[2:(num_random + 1)]
-        names(random_effects_var) = names(getIsoEffectsCounts(model_design))
-        protein_abundance = coef[(num_random + 2):(num_random + num_proteins + 1)]
-        fixed_effects = coef[(num_random + num_proteins + 2):length(coef)]
-
-        fitted = as.numeric(log(getIsoProteinDesign(model_design) %*% exp(protein_abundance)) +
-                                getIsoFixedDesign(model_design) %*% fixed_effects)
-        residuals = as.numeric(response - fitted)
-        message("Computing hessian, please wait...")
-        hessian = get_hessian(model_design, pars)
-        conv_message = "model converged"
-    } else {
-        coef = c(NA, num_fixed + num_random + num_proteins)
-        loglik = NA
-        sigma = NA
-        random_effects_var = rep(NA, num_random)
-        protein_abundance = rep(NA, num_proteins)
-        fixed_effects = rep(NA, num_fixed)
-
-        fitted = rep(NA, getIsoNumObservations(model_design))
-        residuals = rep(NA, getIsoNumObservations(model_design))
-        hessian = matrix()
-        conv_message = "model did not converge"
-    }
-    model_information = list(coef = coef, sigma = sigma,
-                             random_effects_var = random_effects_var,
-                             protein_abundance = protein_abundance,
-                             fixed_effects = fixed_effects,
-                             fitted = fitted, residuals = residuals,
-                             hessian = hessian,
-                             loglik = loglik, message = conv_message)
-}
-
-
-get_hessian = function(model_design, pars) {
-    grad_m = getIsoAnalyticalGradient(model_design)
-    hessian_num = numDeriv::jacobian(grad_m, pars)
-    row.names(hessian_num) = c("sigma", names(getIsoEffectsCounts(model_design)),
-                               colnames(getIsoProteinDesign(model_design)),
-                               colnames(getIsoFixedDesign(model_design)))
-    colnames(hessian_num) = row.names(hessian_num)
-    hessian_num
-}
--- R/utils_log_likelihood.R
+++ R/utils_log_likelihood.R
@@ -1,3 +1,55 @@
+get_iso_full_loglikelihood = function(x) {
+    if (isIsoLinear(x)) {
+        stop("Not implemented yet")
+        # get_iso_full_linear_loglikelihood(x)
+    } else {
+        get_iso_full_nonlinear_loglikelihood(x)
+    }
+}
+
+get_iso_full_nonlinear_loglikelihood = function(model_design) {
+    response = getIsoResponse(model_design)
+    random_effects_counts = getIsoEffectsCounts(model_design)
+    num_random_effects = getIsoNumRandom(model_design)
+    proteins_design = getIsoProteinDesign(model_design)
+    num_proteins = getIsoNumProteins(model_design)
+    fixed_design = getIsoFixedDesign(model_design)
+    num_fixed_effects = getIsoNumFixed(model_design)
+    random_design = getIsoRandomDesign(model_design)
+    independent_unit = getIsoIndependentUnit(model_design)
+
+    neg_loglik = get_neglog_full(random_design, proteins_design, fixed_design,
+                                 response, num_random_effects, num_fixed_effects,
+                                 num_proteins, random_effects_counts,
+                                 independent_unit)
+    neg_loglik
+}
+
+# get_iso_random_loglikelihood = function(x) {
+#     if (isIsoLinear(x)) {
+#         get_iso_random_linear_model(x)
+#     } else {
+#         get_iso_random_nonlinear_model(x)
+#     }
+# }
+#
+# get_iso_fixed_loglikelihood = function(x) {
+#     if (isIsoLinear(x)) {
+#         get_iso_fixed_linear_loglikelihood(x)
+#     } else {
+#         get_iso_fixed_nonlinear_loglikelihood(x)
+#     }
+# }
+#
+# get_iso_protein_loglikelihood = function(x) {
+#     if (isIsoLinear(x)) {
+#         get_iso_protein_linear_loglikelihood(x)
+#     } else {
+#         get_iso_protein_nonlinear_loglikelihood(x)
+#     }
+# }
+
+
 # FULL MODEL
 get_neglog_full_unit = function(random_design, proteins_design, fixed_design,
                                 response, num_random_effects, num_fixed_effects,

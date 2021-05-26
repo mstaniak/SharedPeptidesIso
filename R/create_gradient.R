d7ee8a401c48a8a872b5a4cdeadd1f4c0ed14091
mstaniak <mtst@mstaniak.pl>
2021-05-26 11:37:05 +0200

    updated gradient/loglik functions

--- R/create_gradient.R
+++ R/create_gradient.R
@@ -1,31 +1,31 @@
-setGeneric("getIsoLogLikelihood",
-           function(x) standardGeneric("getIsoLogLikelihood"))
-setMethod("getIsoLogLikelihood", "IsoAPQModelDesign",
+setGeneric("getIsoAnalyticalGradient",
+           function(x) standardGeneric("getIsoAnalyticalGradient"))
+setMethod("getIsoAnalyticalGradient", "IsoAPQModelDesign",
           function(x) {
               if (hasIsoFixedEffects(x) & hasIsoRandomEffects(x)) {
-                  get_iso_full_loglikelihood(x)
+                  get_iso_full_gradient(x)
               } else if (!hasIsoFixedEffects(x) & hasIsoRandomEffects(x)) {
                   stop("Not implemented yet")
-                  # get_iso_random_loglikelihood(x, response)
+                  # get_iso_random_gradient(x, response)
               } else if (hasIsoFixedEffects(x) & !hasIsoRandomEffects(x)) {
                   stop("Not implemented yet")
-                  # get_iso_fixed_loglikelihood(x, response)
+                  # get_iso_fixed_gradient(x, response)
               } else {
                   stop("Not implemented yet")
-                  # get_iso_protein_loglikelihood(x, response)
+                  # get_iso_protein_gradient(x, response)
               }
           })
 
-get_iso_full_loglikelihood = function(x) {
+get_iso_full_gradient = function(x) {
     if (isIsoLinear(x)) {
+        # get_iso_full_linear_loglikelihood(x, response)
         stop("Not implemented yet")
-        # get_iso_full_linear_loglikelihood(x)
     } else {
-        get_iso_full_nonlinear_loglikelihood(x)
+        get_iso_full_nonlinear_gradient(x)
     }
 }
 
-get_iso_full_nonlinear_loglikelihood = function(model_design) {
+get_iso_full_nonlinear_gradient = function(model_design) {
     response = getIsoResponse(model_design)
     random_effects_counts = getIsoEffectsCounts(model_design)
     num_random_effects = getIsoNumRandom(model_design)
@@ -36,14 +36,14 @@ get_iso_full_nonlinear_loglikelihood = function(model_design) {
     random_design = getIsoRandomDesign(model_design)
     independent_unit = getIsoIndependentUnit(model_design)
 
-    neg_loglik = get_neglog_full(random_design, proteins_design, fixed_design,
+    gradient = get_gradient_full(random_design, proteins_design, fixed_design,
                                  response, num_random_effects, num_fixed_effects,
                                  num_proteins, random_effects_counts,
                                  independent_unit)
-    neg_loglik
+    gradient
 }
 
-# get_iso_random_loglikelihood = function(x) {
+# get_iso_random_gradient = function(x) {
 #     if (isIsoLinear(x)) {
 #         get_iso_random_linear_model(x)
 #     } else {
